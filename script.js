abb62f981160de1037df48090b43a45d55fe3d92
Niclasp1501 <niclaspflug@gmail.com>
2024-03-31 01:18:06 +0100

    Fertiger Countdown

--- script.js
+++ script.js
@@ -33,3 +33,30 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+// Countdowm Timer
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const countdownDate = new Date("September 14, 2024 16:00:00").getTime();
+
+    const x = setInterval(function() {
+        const now = new Date().getTime();
+        const distance = countdownDate - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "Unser großer Tag ist angekommen!";
+        }
+    }, 1000);
+});
